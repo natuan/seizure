12db64be73cd10139525244133b66937f9de6be7
Tuan Nguyen <atnhan@gmail.com>
2018-01-10 06:38:28 -0500

    Unit autoencoder

---
+++ src/Autoencoder.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+class Autoencoder:
+    def __init__(self,
+                 hidden_activation = tf.nn.elu,
+                 output_activation = None,
+                 regularizer = tf.contrib.layers.l2_regularizer(0.0005),
+                 initializer = tf.contrib.layers.variance_scaling_initializer(),
+                 optimizer = tf.train.AdamOptimizer(0.01)):
+        self.hidden_activation = hidden_activation
+        self.output_activation = output_activation
+        self.regularizer = regularizer
+        self.initializer = initializer
+        self.optimizer = optimizer
+        
